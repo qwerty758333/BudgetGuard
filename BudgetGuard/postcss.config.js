fddda5668f7d6e0f75eb424b2c2236b62501d6a9
qwerty758333 <irangapipi@gmail.com>
2026-05-16 15:43:46 +0530

    "chore: debugging for deploy"

--- BudgetGuard/postcss.config.js
+++ BudgetGuard/postcss.config.js
@@ -1,10 +1,5 @@
 export default {
   plugins: {
-<<<<<<< HEAD
-    tailwindcss: {},
-    autoprefixer: {},
-=======
     '@tailwindcss/postcss': {},
->>>>>>> 48b87abb7adbb8039e2fe8d3cec8cef1d9f847bd
   },
 }
--- BudgetGuard/tailwind.config.js
+++ BudgetGuard/tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
-=======
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
->>>>>>> 48b87abb7adbb8039e2fe8d3cec8cef1d9f847bd
   theme: {
     extend: {},
   },
